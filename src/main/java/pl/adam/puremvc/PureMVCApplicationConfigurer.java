2ce8eacd50a78ff4289fc3e4e843e945647ca5c4
robert <nowak021@gmail.com>
2017-11-02 18:30:30 +0100

    dodanie configa MVC

--- src/main/java/pl/adam/puremvc/PureMVCApplicationConfigurer.java
+++ src/main/java/pl/adam/puremvc/PureMVCApplicationConfigurer.java
@@ -1,23 +1,28 @@
 package pl.adam.puremvc;
 
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 /**
  * Created by SkyNET on 2017-11-01 Klasa służąca do inicjalizacji servletu.
- *
  */
-public class PureMVCApplicationConfigurer extends AbstractAnnotationConfigDispatcherServletInitializer {
+public class PureMVCApplicationConfigurer implements WebApplicationInitializer {
 
-    protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
+    public void onStartup(ServletContext container) throws ServletException {
+
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(PureMVCWebConfiguration.class);
+        ctx.setServletContext(container);
+
+        ServletRegistration.Dynamic servlet = container.addServlet("dispatcher", new DispatcherServlet(ctx));
+
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/");
     }
 
-    protected Class<?>[] getServletConfigClasses() {
-        return new Class[]{PureMVCWebConfiguration.class};
-    }
-
-    protected String[] getServletMappings() {
-        System.out.println("JRJRJRJRJRJRJRJRJ");
-        return new String[]{"/"};
-    }
 }
--- src/main/java/pl/adam/puremvc/PureMVCWebConfiguration.java
+++ src/main/java/pl/adam/puremvc/PureMVCWebConfiguration.java
@@ -2,13 +2,14 @@ package pl.adam.puremvc;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * Created by SkyNET on 2017-11-01.
  */
 
 @Configuration
+@EnableWebMvc
 @ComponentScan("pl.adam.puremvc")
 public class PureMVCWebConfiguration {
-
 }
