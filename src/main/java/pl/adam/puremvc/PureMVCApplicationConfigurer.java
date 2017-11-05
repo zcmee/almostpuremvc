a611aea2a14855e8454490f66ff1313ce7a95fd9
ADam <zcm@interia.pl>
2017-11-05 20:29:37 +0100

    Zmiana sposobu wczytywania Inicjalizacji Beana.

--- src/main/java/pl/adam/puremvc/PureMVCApplicationConfigurer.java
+++ src/main/java/pl/adam/puremvc/PureMVCApplicationConfigurer.java
@@ -1,23 +1,47 @@
 package pl.adam.puremvc;
 
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 /**
  * Created by SkyNET on 2017-11-01 Klasa służąca do inicjalizacji servletu.
  *
  */
+//public class PureMVCApplicationConfigurer implements WebApplicationInitializer {
+//
+//    public void onStartup(ServletContext container) throws ServletException {
+//
+//        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+//        ctx.register(PureMVCWebConfiguration.class);
+//        ctx.setServletContext(container);
+//        ServletRegistration.Dynamic servlet = container.addServlet("dispatcher", new DispatcherServlet(ctx));
+//        servlet.setLoadOnStartup(1);
+//        servlet.addMapping("/");
+//    }
+//}
+
 public class PureMVCApplicationConfigurer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
-    protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
+    protected String[] getServletMappings() {
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!getServletMappings()");
+        return new String[]{"/"};
     }
+
+    protected Class<?>[] getRootConfigClasses() {
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!getRootConfigClasses");
+        return new Class[]{PureMVCAppConfiguration.class};
+    }
+
 
     protected Class<?>[] getServletConfigClasses() {
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!getServletConfigClasses()");
         return new Class[]{PureMVCWebConfiguration.class};
-    }
-
-    protected String[] getServletMappings() {
-        System.out.println("JRJRJRJRJRJRJRJRJ");
-        return new String[]{"/"};
+//        return null;
     }
 }
