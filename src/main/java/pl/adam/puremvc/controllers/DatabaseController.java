f4fd3399b7978a5c6e107145185866e5d5a97235
ADam <zcm@interia.pl>
2017-12-07 23:12:32 +0100

    Dodanie sortowania i paginacji stron w spring data

--- src/main/java/pl/adam/puremvc/controllers/DatabaseController.java
+++ src/main/java/pl/adam/puremvc/controllers/DatabaseController.java
@@ -1,7 +1,11 @@
 package pl.adam.puremvc.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -35,6 +39,33 @@ public class DatabaseController {
     public List<User> getUser() {
         List<User> users = userRespository.findAll();
         return users;
+    }
+
+    @GetMapping("sortedbyfirstname")
+    public List<User> getsortedByUserByFirstName() {
+        List<User> users = userRespository.findAll(new Sort(Sort.Direction.ASC, "firstName"));
+        return users;
+    }
+
+    @GetMapping("sortedbylastname")
+    public List<User> getsortedUserByLastName() {
+        List<User> users = userRespository.findAll(new Sort(Sort.Direction.ASC, "lastName"));
+        return users;
+    }
+
+    @GetMapping("sortedbyfirstnameandlastname")
+    public List<User> getsortedBUserByLastName() {
+        List<User> users = userRespository.findAll(new Sort(
+                                                        new Sort.Order(Sort.Direction.ASC, "firstName"),
+                                                        new Sort.Order(Sort.Direction.ASC, "lastName")
+                                                    ));
+        return users;
+    }
+
+    @GetMapping("sortedpagination/{page}")
+    public List<User> sortedpagination(@PathVariable Integer page) {
+        Page<User> pageOfUsers = userRespository.findAll(new PageRequest(page, 2));
+        return pageOfUsers.getContent();
     }
 
 }
--- src/main/java/pl/adam/puremvc/respositories/UserRespository.java
+++ src/main/java/pl/adam/puremvc/respositories/UserRespository.java
@@ -1,5 +1,7 @@
 package pl.adam.puremvc.respositories;
 
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import pl.adam.puremvc.entities.User;
 
@@ -9,10 +11,11 @@ import java.util.List;
  * Created by SkyNET on 2017-11-12.
  */
 
-public interface UserRespository extends CrudRepository<User, Integer> {
+public interface UserRespository extends JpaRepository<User, Integer> {
     List<User> findAll();
     User findById(Integer id);
     User save(User user);
+    List<User> findAll(Sort firstName);
 }
 
 
