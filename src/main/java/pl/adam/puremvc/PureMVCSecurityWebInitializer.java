a5949d16ba272bea80166bd4854e859d77d132aa
ADam <zcm@interia.pl>
2017-12-11 00:22:17 +0100

    Dodanie klasy inicjalizacyjnej oraz konfiguracyjnej. Od tej wstępnej konfiguracji, wszystkie żądanie przebiega przez spring security(filtry), plus dostajemy obrone przed niektórymi atakami(crsf, session fixation). Aby użytkownik cos wykonał musi sie zautentykwoać

---
+++ src/main/java/pl/adam/puremvc/PureMVCSecurityWebInitializer.java
@@ -0,0 +1,10 @@
+package pl.adam.puremvc;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Created by SkyNET on 2017-12-10.
+ */
+
+public class PureMVCSecurityWebInitializer extends AbstractSecurityWebApplicationInitializer {
+}
---
+++ src/main/java/pl/adam/puremvc/PureMVCWebSecurityConfiguration.java
@@ -0,0 +1,12 @@
+package pl.adam.puremvc;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by SkyNET on 2017-12-10.
+ */
+
+@EnableWebSecurity
+public class PureMVCWebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+}
