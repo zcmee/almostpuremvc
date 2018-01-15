bd493dc44c26d02d602985c304d6c5af3032c0fa
ADam <zcm@interia.pl>
2018-01-15 23:15:20 +0100

    Dodanie użytkownika do pamięci w spring security

--- src/main/java/pl/adam/puremvc/PureMVCWebSecurityConfiguration.java
+++ src/main/java/pl/adam/puremvc/PureMVCWebSecurityConfiguration.java
@@ -1,12 +1,21 @@
 package pl.adam.puremvc;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-/**
- * Created by SkyNET on 2017-12-10.
- */
 
 @EnableWebSecurity
 public class PureMVCWebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("yoyne").password("666").roles("user").build());
+        return manager;
+    }
 }
